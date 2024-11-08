2a86f23efad13d043092c9fc729d64af651acb2b
Shodmonov01 <a.shodmonov60@gmail.com>
2024-11-08 11:18:44 +0500

    clean code 1

--- src/api/api.js
+++ src/api/api.js
@@ -1,23 +1,3 @@
-// export const BASE_URL = 'http://51.250.22.154:8000';
-
-// import axios from 'axios';
-
-// export const getAccount = axios.create({
-//   baseURL: `${BASE_URL}/api/account/v1`,
-//   headers: {
-//     'Content-Type': 'application/json',
-//   },
-// });
-
-
-// export const getCompany = axios.create({
-//   baseURL: `${BASE_URL}/api/account/v1`,
-//   headers: {
-//     'Content-Type': 'application/json',
-//   },
-// });
-
-
 import axios from 'axios';
 
 // export const BASE_URL = 'http://51.250.22.154:8000';
